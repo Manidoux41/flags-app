2f4eacbabc8df0a8693ad3b69fb98398afcd7e1f
Manidoux41 <lherissondu41@gmail.com>
2023-01-04 07:11:01 +0100

    first commit

---
+++ src/components/Logo.js
@@ -0,0 +1,10 @@
+const Logo = () => {
+  return (
+    <div className="logo">
+        <img src="./logo192.png" alt="logo react" />
+        <h3>React World</h3>
+    </div>
+  )
+}
+
+export default Logo
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Logo from '../components/Logo'
+import Navigation from '../components/Navigation'
+
+const About = () => {
+  return (
+    <div>
+        <Logo />
+        <Navigation />
+        <h1>A Propos</h1>
+        <br />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex repudiandae praesentium consequuntur quod sequi quidem aliquam, quaerat cum nesciunt hic eos repellendus eius sed accusantium similique provident impedit architecto atque ab itaque? Laudantium quas eius doloribus. Autem delectus sapiente velit deleniti animi error perferendis nihil expedita repudiandae. Sequi, molestias voluptatum veniam placeat odit distinctio voluptatem quo iusto suscipit quas consectetur eveniet velit totam numquam? Ipsa mollitia qui voluptate eveniet expedita ut voluptates asperiores porro, et ducimus est, aliquid dolorum debitis fuga harum error illo perferendis. Beatae ab, sequi iure molestias mollitia blanditiis aspernatur delectus consequatur dolores dicta, sint adipisci odio!</p>
+        <br />
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus eos laudantium, autem, error, quas corporis ut sunt necessitatibus iusto voluptatum eligendi velit sit accusamus. Odit illo ratione sed suscipit enim, earum sequi neque dicta porro eius iusto vitae dolor ex provident unde? A cumque quia molestiae optio magni eum odio explicabo non aperiam fuga nam dolorum enim mollitia deserunt autem, quasi voluptates, cum at cupiditate. Adipisci ipsum vitae blanditiis sint.</p>
+    </div>
+  )
+}
+
+export default About
